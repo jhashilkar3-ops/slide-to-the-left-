541dc86d493495c7c2e4484141e7e6d893c62389
Jiya Hashilkar <jhashilkar3@gmail.com>
2026-03-21 20:11:57 -0500

    Update slide.hpp

--- src/slide.hpp
+++ src/slide.hpp
@@ -1,3 +1,14 @@
-#include <string>
+#ifndef slide_HPP
+#define slide_HPP
 
-std::string slide();
+#include <vector>
+
+void arrayShift(int arr[], int size, int shifts);
+
+void arrayShiftLeft(int arr[], int size, int shifts);
+
+void arrayShiftRight(int arr[], int size, int shifts);
+
+void vectorShift(std::vector<int>& vec, int shifts);
+
+#endif
